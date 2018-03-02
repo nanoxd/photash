2bce2139f4fa116ddc0b78b11312e3604ae1668a
Fernando Paredes <nano@fdp.io>
2018-03-01 19:53:48 -0500

    ✨ Add image_hash

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate libc;
+use libc::{c_char, c_int, uint64_t};
+use std::ffi::CString;
+
+#[link(name = "pHash")]
+extern "C" {
+    fn ph_dct_imagehash(file: *const c_char, hash: *mut uint64_t) -> c_int;
+}
+
+fn image_hash(file: &str) -> Option<u64> {
+    let mut hash: u64 = 0;
+    let cstr = CString::new(file).unwrap();
+    unsafe {
+        let ptr = cstr.as_ptr();
+        match ph_dct_imagehash(ptr, &mut hash) {
+            0 => Some(hash),
+            _ => None,
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
