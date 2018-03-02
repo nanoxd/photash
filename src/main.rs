2d31672e27a6a3bd2c9c5c1f91c33ca38b68aab3
Fernando Paredes <nano@fdp.io>
2018-03-01 20:02:52 -0500

    ⚡ Use rayon to parallelize calculations

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 extern crate libc;
+extern crate rayon;
 use libc::{c_char, c_int, uint64_t};
 use std::ffi::CString;
 use std::env;
+use rayon::prelude::*;
 
 #[link(name = "pHash")]
 extern "C" {
@@ -21,10 +23,10 @@ fn image_hash(file: &str) -> Option<u64> {
 }
 
 fn main() {
-    env::args()
-        .skip(1)
-        .for_each(|file| match image_hash(&file) {
-            Some(hash) => println!("{:016x} {}", hash, file),
-            None => eprintln!("Failed to hash {}", file),
-        })
+    let files: Vec<_> = env::args().skip(1).collect();
+
+    files.par_iter().for_each(|file| match image_hash(&file) {
+        Some(hash) => println!("{:016x} {}", hash, file),
+        None => eprintln!("Failed to hash {}", file),
+    })
 }
